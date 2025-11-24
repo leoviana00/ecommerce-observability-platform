0ce5fca43510e1b0cf5587f1a61a3a5c5caad504
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2025-11-24 12:08:09 -0300

    feat(app): Classe de Inicialização da Aplicação

--- ecommerce-stack/ecommerce-gateway/src/main/java/io/viana/ecommerce_gateway/EcommerceGatewayApplication.java
+++ ecommerce-stack/ecommerce-gateway/src/main/java/io/viana/ecommerce_gateway/EcommerceGatewayApplication.java
@@ -1,12 +1,21 @@
 package io.viana.ecommerce_gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Marca a classe como uma aplicação Spring Boot. 
+// Esta anotação é a combinação de @Configuration, @EnableAutoConfiguration e @ComponentScan.
 @SpringBootApplication
 public class EcommerceGatewayApplication {
 
+    /**
+     * O método principal (main) que inicia a aplicação Spring Boot.
+     * @param args Argumentos de linha de comando.
+     */
     public static void main(String[] args) {
+        // Método estático que configura e inicia a aplicação.
         SpringApplication.run(EcommerceGatewayApplication.class, args);
     }
 }
